684c8887012cbcd86747279f0f0638659f051b71
Jose F. Morales <jfmcjf@gmail.com>
2021-03-21 01:58:18 +0100

    Bump version to 1.20
    Src-commit: 68ef9e8b0483d0eeecd9c15347228a0e47d6bdba

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(cleandirs).
 version('1.0').
 depends([
-    core-[version>='1.19']
+    core-[version>='1.20']
 ]).
 cmd('cmds/cleandirs').
 manual('cleandirs', [main='doc/SETTINGS.pl']).
