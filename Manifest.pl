3680e4bf5e58a51388fb6029aa900df66466570f
Jose F. Morales <jfmcjf@gmail.com>
2022-03-03 09:23:43 +0100

    Updated versions
    Src-commit: 471a8d28f155296b6951c2c611c0f2aad1a3c8c7

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(cleandirs).
 version('1.0').
 depends([
-    core-[version>='1.20']
+    core-[version>='1.21']
 ]).
 cmd('cmds/cleandirs').
 manual('cleandirs', [main='doc/SETTINGS.pl']).
