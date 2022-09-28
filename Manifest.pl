4a5f6b9edede5010806690430f8d73f7a14f28ff
Jose F. Morales <jfmcjf@gmail.com>
2022-09-28 17:42:18 +0200

    Updating version 1.22.0
    Src-commit: 3b5f0895bab4700718bc79deed54fffdc1dcd374

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(cleandirs).
 version('1.0').
 depends([
-    core-[version>='1.21']
+    core-[version>='1.22']
 ]).
 cmd('cmds/cleandirs').
 manual('cleandirs', [main='doc/SETTINGS.pl']).
