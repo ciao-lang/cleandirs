1748ae54d8e689881ee12df4f430614f1b01808f
Jose F. Morales <jfmcjf@gmail.com>
2024-10-13 16:02:39 -0500

    Bump Ciao version to 1.24
    Src-commit: 8123d52c941a1c2f134e2d6fcc68db13b9c462bb

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(cleandirs).
 version('1.0').
 depends([
-    core-[version>='1.23']
+    core-[version>='1.24']
 ]).
 cmd('cmds/cleandirs').
 manual('cleandirs', [main='doc/SETTINGS.pl']).
