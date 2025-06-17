8e869993eb45038d2127046d2315a337ede99883
Jose F. Morales <jfmcjf@gmail.com>
2025-06-17 17:47:28 +0200

    Jump version to 1.25
    Src-commit: 1174c3364d0d3ec61f21ac80fdf5116172d7acb8

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(cleandirs).
 version('1.0').
 depends([
-    core-[version>='1.24']
+    core-[version>='1.25']
 ]).
 cmd('cmds/cleandirs').
 manual('cleandirs', [main='doc/SETTINGS.pl']).
